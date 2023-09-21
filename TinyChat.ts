ef0d181521aef3bdb4e275dbde2e12a115ae9267
bleesman21 <bradley.leesman@slu.edu>
2023-09-21 14:52:21 -0500

    Adding Sender in Groups

--- TinyChat.ts
+++ TinyChat.ts
@@ -352,6 +352,14 @@ peer.on('connection', (dataConnection: DataConnection): void => dataConnection.o
 				event: MessageDataEvent.Delivered,
 			});
 			paragraph.id = messageData.id;
+			if (split.length > 1) {
+				const from: HTMLParagraphElement = document.createElement('p');
+				from.innerHTML = `<small><small>From: ${trueFrom}</small></small>`;
+				if (el.lastChild && (el.lastChild as Element).className === 'typing')
+					el.insertBefore(from, el.lastChild);
+				else
+					el.insertAdjacentElement('beforeend', from);
+			}
 			if (messageData.prev) {
 				const reply: HTMLParagraphElement = document.createElement('p');
 				reply.innerHTML = `<small><small>${(document.getElementById(new TextDecoder().decode(await window.crypto.subtle.decrypt(
@@ -487,7 +495,7 @@ const createChat: (to: string, establishKey: boolean) => Promise<HTMLSpanElement
 const send: (to: string, messageData: MessageData, isFirst?: boolean) => void = (to: string, messageData: MessageData, isFirst: boolean = true): void => {
 	const localEdit: string | undefined = editing;
 	const split: Array<string> = messageData.from.split(',');
-	split[0] = to;
+	const trueFrom: string = split[0];
 	const aesAccess: string = (split as any).toSorted().join(',');
 	const conn: DataConnection = peer.connect(to);
 	conn.on('open', async (): Promise<void> => {
