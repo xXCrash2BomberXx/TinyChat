f3890104e87bf2280cf8f5d0fdb5d59bb4977a7e
bleesman21 <bradley.leesman@slu.edu>
2023-10-03 18:23:00 -0500

    Added Manual 'toSorted' Override for Older Browsers

--- TinyChat.ts
+++ TinyChat.ts
@@ -1,3 +1,6 @@
+if (!Array.prototype.toSorted)
+	Array.prototype.toSorted = function (compareFn?: ((a: any, b: any) => number) | undefined): Array<any> { return [...this].sort(compareFn); }
+
 /**
  * Message event used in {@link MessageData.event}.
  * @readonly
