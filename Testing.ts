43ebed427b9daadbb88acfbb5a7c54f93db59213
bleesman21 <89619369+bleesman21@users.noreply.github.com>
2023-10-25 01:59:38 -0500

    Adding Unit Test (#68)
    * Generates ID
    
    * Completes Setup
    
    * Unit Test Done
    
    * Unit Test Done

--- Testing.ts
+++ Testing.ts
@@ -1,24 +1,28 @@
 const { Client } = require('./TinyChat.js');
 const { JSDOM } = require('jsdom');
 const localCrypto = new (require("@peculiar/webcrypto").Crypto)();
+const { readFileSync } = require('fs');
 
-var polyfills: any = {
+const polyfills: any = {
 	fetch: import('node-fetch'),
 	WebSocket: require('ws'),
 	WebRTC: require('wrtc'),
 	FileReader: require('filereader')
 };
 
-const generateClient: () => typeof Client = () => new Client(JSDOM.fromFile('./TinyChat.html').window, localCrypto, polyfills);
+const generateClient: () => typeof Client = () => new Client(new JSDOM(readFileSync('./TinyChat.html')).window, localCrypto, localCrypto.randomUUID(), polyfills);
 
-for (const [key, value] of Object.entries({
-	createChatTest: (): boolean => {
+Promise.all(Object.entries({
+	'createChatTest': async (): Promise<boolean> => {
 		const client: typeof Client = generateClient();
-		return true;
+		const UID: string = localCrypto.randomUUID();
+		await client.createChat(UID, false);
+		return client.getDocument().getElementById(UID).parentElement.outerHTML === `<details open=""><summary>${UID}</summary><div class="chatButtonsContainer"><input type="button" value="Clear Chat Locally" class="chatButtons"><input type="button" value="Clear Chat Globally" class="chatButtons"><input type="button" value="Generate New AES Key" class="chatButtons"></div><span class="message" id="${UID}"></span><input type="text" class="sendBar"></details>`;
 	}
-}))
-	if (!value()) {
+}).map(async ([key, value]: [string, () => Promise<boolean>]): Promise<void> => {
+	if (!await value()) {
 		console.error(`Failed Test: ${key}`);
 		process.exit(1);
-	}
-process.exit(0);
+	} else
+		console.log(`Passed Test: ${key}`);
+})).then(() => process.exit(0));
