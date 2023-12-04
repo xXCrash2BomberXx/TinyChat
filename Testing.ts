ca1910d3c9d382132abcf96014ccfb160325f55d
bleesman21 <89619369+bleesman21@users.noreply.github.com>
2023-12-04 13:25:03 -0600

    Group tests (#120)
    * Basic Received
    
    * Create Group Chat Test
    
    * Group Typing Indicator
    
    * Group Typing Indicator
    
    * Stop Typing Indicators

--- Testing.ts
+++ Testing.ts
@@ -876,6 +876,184 @@ Promise.all(Object.entries({
 		return client.window.document.getElementById(UUID).parentElement.outerHTML === `<details open=""><summary>${UUID}</summary><div class="chatButtonsContainer"><input type="button" value="Clear Chat Locally" class="chatButtons"><input type="button" value="Clear Chat Globally" class="chatButtons"><input type="button" value="Generate New AES Key" class="chatButtons"><input type="button" value="Upload File" class="chatButtons"><input type="button" value="Share Location" class="chatButtons"><label>Send Typing Indicators</label><input type="checkbox" class="chatButtons"></div><span class="message" id="${UUID}"><p class="received" id="${messageID}">${messageBody} <small><small><small><i>${messageTime}</i></small></small></small> <small><small><small><i>✓</i></small></small></small><p class="received" id="${messageID2}"><p class="receivedReply" id="${messageID}"><small><small>${messageBody} <small><small><small><i>${messageTime}</i></small></small></small>   <small><small><small><i>✓</i></small></small></small></small></small></p>${messageBody2} <small><small><small><i>${messageTime2}</i></small></small></small> <small><small><small><i>✓</i></small></small></small></p></p></span><div class="chatButtonsContainer"><textarea class="sendBar"></textarea><input type="button" value=">" class="sendButton"></div></details>`;
 	},
 	*/
+	'createGroupChatTest': async (): Promise<boolean> => {
+		const client: typeof Client = generateClient();
+		const UUID: string = client.randomUUID();
+		const UUID2: string = client.randomUUID();
+		//@ts-ignore: 2339
+		const aesAccess: string = [UUID, UUID2].toSorted().join(',');
+		await client.createChat(`${UUID}, ${UUID2}`);
+		return client.window.document.getElementById(aesAccess).parentElement.outerHTML === `<details open=""><summary>${aesAccess}</summary><div class="chatButtonsContainer"><input type="button" value="Clear Chat Locally" class="chatButtons"><input type="button" value="Clear Chat Globally" class="chatButtons"><input type="button" value="Generate New AES Key" class="chatButtons"><input type="button" value="Upload File" class="chatButtons"><input type="button" value="Share Location" class="chatButtons"><label>Send Typing Indicators</label><input type="checkbox" class="chatButtons"></div><span class="message" id="${aesAccess}"></span><div class="chatButtonsContainer"><textarea class="sendBar"></textarea><input type="button" value=">" class="sendButton"></div></details>`;
+	},
+	'basicGroupMessageReceived': async (): Promise<boolean> => {
+		const client: typeof Client = generateClient();
+		const UUID: string = client.randomUUID();
+		const UUID2: string = client.randomUUID();
+		//@ts-ignore: 2339
+		const aesAccess: string = [UUID, UUID2].toSorted().join(',');
+		await client.createChat(`${UUID}, ${UUID2}`);
+		const messageBody: string = 'test message';
+		const messageTime: string = new Date().toLocaleTimeString();
+		const messageID: string = client.randomUUID();
+		await client.render(client.id, {
+			from: `${UUID},${UUID2}`,
+			body: await client.encryptAES(aesAccess, messageBody),
+			time: await client.encryptAES(aesAccess, messageTime),
+			id: messageID,
+			event: undefined,
+			prev: undefined
+		});
+		const messageBody2: string = 'test message 2';
+		const messageTime2: string = new Date().toLocaleTimeString();
+		const messageID2: string = client.randomUUID();
+		await client.render(client.id, {
+			from: `${UUID2},${UUID}`,
+			body: await client.encryptAES(aesAccess, messageBody2),
+			time: await client.encryptAES(aesAccess, messageTime2),
+			id: messageID2,
+			event: undefined,
+			prev: undefined
+		});
+		return client.window.document.getElementById(aesAccess).parentElement.outerHTML === `<details open=""><summary>${aesAccess}</summary><div class="chatButtonsContainer"><input type="button" value="Clear Chat Locally" class="chatButtons"><input type="button" value="Clear Chat Globally" class="chatButtons"><input type="button" value="Generate New AES Key" class="chatButtons"><input type="button" value="Upload File" class="chatButtons"><input type="button" value="Share Location" class="chatButtons"><label>Send Typing Indicators</label><input type="checkbox" class="chatButtons"></div><span class="message" id="${aesAccess}"><p class="received" id="${messageID}"><small><small><small><u>${UUID}</u></small></small></small><br>${messageBody} <small><small><small><i>${messageTime}</i></small></small></small> <small><small><small><i>✓</i></small></small></small></p><p class="received" id="${messageID2}"><small><small><small><u>${UUID2}</u></small></small></small><br>${messageBody2} <small><small><small><i>${messageTime2}</i></small></small></small> <small><small><small><i>✓</i></small></small></small></p></span><div class="chatButtonsContainer"><textarea class="sendBar"></textarea><input type="button" value=">" class="sendButton"></div></details>`;
+	},
+	'basicGroupMessageTypingReceived': async (): Promise<boolean> => {
+		const client: typeof Client = generateClient();
+		const UUID: string = client.randomUUID();
+		const UUID2: string = client.randomUUID();
+		//@ts-ignore: 2339
+		const aesAccess: string = [UUID, UUID2].toSorted().join(',');
+		await client.createChat(`${UUID}, ${UUID2}`);
+		await client.render(client.id, {
+			from: `${UUID},${UUID2}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.Typing,
+			prev: undefined
+		});
+		const messageBody: string = 'test message';
+		const messageTime: string = new Date().toLocaleTimeString();
+		const messageID: string = client.randomUUID();
+		await client.render(client.id, {
+			from: `${UUID},${UUID2}`,
+			body: await client.encryptAES(aesAccess, messageBody),
+			time: await client.encryptAES(aesAccess, messageTime),
+			id: messageID,
+			event: undefined,
+			prev: undefined
+		});
+		await client.render(client.id, {
+			from: `${UUID2},${UUID}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.Typing,
+			prev: undefined
+		});
+		const messageBody2: string = 'test message 2';
+		const messageTime2: string = new Date().toLocaleTimeString();
+		const messageID2: string = client.randomUUID();
+		await client.render(client.id, {
+			from: `${UUID2},${UUID}`,
+			body: await client.encryptAES(aesAccess, messageBody2),
+			time: await client.encryptAES(aesAccess, messageTime2),
+			id: messageID2,
+			event: undefined,
+			prev: undefined
+		});
+		return client.window.document.getElementById(aesAccess).parentElement.outerHTML === `<details open=""><summary>${aesAccess}</summary><div class="chatButtonsContainer"><input type="button" value="Clear Chat Locally" class="chatButtons"><input type="button" value="Clear Chat Globally" class="chatButtons"><input type="button" value="Generate New AES Key" class="chatButtons"><input type="button" value="Upload File" class="chatButtons"><input type="button" value="Share Location" class="chatButtons"><label>Send Typing Indicators</label><input type="checkbox" class="chatButtons"></div><span class="message" id="${aesAccess}"><p class="received" id="${messageID}"><small><small><small><u>${UUID}</u></small></small></small><br>${messageBody} <small><small><small><i>${messageTime}</i></small></small></small> <small><small><small><i>✓</i></small></small></small></p><p class="received" id="${messageID2}"><small><small><small><u>${UUID2}</u></small></small></small><br>${messageBody2} <small><small><small><i>${messageTime2}</i></small></small></small> <small><small><small><i>✓</i></small></small></small></p></span><div class="chatButtonsContainer"><textarea class="sendBar"></textarea><input type="button" value=">" class="sendButton"></div></details>`;
+	},
+	'typingGroupIndicatorReceived': async (): Promise<boolean> => {
+		const client: typeof Client = generateClient();
+		const UUID: string = client.randomUUID();
+		const UUID2: string = client.randomUUID();
+		//@ts-ignore: 2339
+		const aesAccess: string = [UUID, UUID2].toSorted().join(',');
+		await client.createChat(`${UUID}, ${UUID2}`);
+		await client.render(client.id, {
+			from: `${UUID},${UUID2}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.Typing,
+			prev: undefined
+		});
+		await client.render(client.id, {
+			from: `${UUID2},${UUID}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.Typing,
+			prev: undefined
+		});
+		return client.window.document.getElementById(aesAccess).parentElement.outerHTML === `<details open=""><summary>${aesAccess}</summary><div class="chatButtonsContainer"><input type="button" value="Clear Chat Locally" class="chatButtons"><input type="button" value="Clear Chat Globally" class="chatButtons"><input type="button" value="Generate New AES Key" class="chatButtons"><input type="button" value="Upload File" class="chatButtons"><input type="button" value="Share Location" class="chatButtons"><label>Send Typing Indicators</label><input type="checkbox" class="chatButtons"></div><span class="message" id="${aesAccess}"><p class="typing">${UUID} is Typing...</p><p class="typing">${UUID2} is Typing...</p></span><div class="chatButtonsContainer"><textarea class="sendBar"></textarea><input type="button" value=">" class="sendButton"></div></details>`;
+	},
+	'stopTypingGroupIndicatorReceived': async (): Promise<boolean> => {
+		const client: typeof Client = generateClient();
+		const UUID: string = client.randomUUID();
+		const UUID2: string = client.randomUUID();
+		//@ts-ignore: 2339
+		const aesAccess: string = [UUID, UUID2].toSorted().join(',');
+		await client.createChat(`${UUID}, ${UUID2}`);
+		await client.render(client.id, {
+			from: `${UUID},${UUID2}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.Typing,
+			prev: undefined
+		});
+		await client.render(client.id, {
+			from: `${UUID2},${UUID}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.Typing,
+			prev: undefined
+		});
+		await client.render(client.id, {
+			from: `${UUID},${UUID2}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.StopTyping,
+			prev: undefined
+		});
+		await client.render(client.id, {
+			from: `${UUID2},${UUID}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.StopTyping,
+			prev: undefined
+		});
+		return client.window.document.getElementById(aesAccess).parentElement.outerHTML === `<details open=""><summary>${aesAccess}</summary><div class="chatButtonsContainer"><input type="button" value="Clear Chat Locally" class="chatButtons"><input type="button" value="Clear Chat Globally" class="chatButtons"><input type="button" value="Generate New AES Key" class="chatButtons"><input type="button" value="Upload File" class="chatButtons"><input type="button" value="Share Location" class="chatButtons"><label>Send Typing Indicators</label><input type="checkbox" class="chatButtons"></div><span class="message" id="${aesAccess}"></span><div class="chatButtonsContainer"><textarea class="sendBar"></textarea><input type="button" value=">" class="sendButton"></div></details>`;
+	},
+	'stopTypingGroupIndicatorNoTypingReceived': async (): Promise<boolean> => {
+		const client: typeof Client = generateClient();
+		const UUID: string = client.randomUUID();
+		const UUID2: string = client.randomUUID();
+		//@ts-ignore: 2339
+		const aesAccess: string = [UUID, UUID2].toSorted().join(',');
+		await client.createChat(`${UUID}, ${UUID2}`);
+		await client.render(client.id, {
+			from: `${UUID},${UUID2}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.StopTyping,
+			prev: undefined
+		});
+		await client.render(client.id, {
+			from: `${UUID2},${UUID}`,
+			body: '',
+			time: '',
+			id: '',
+			event: MessageDataEvent.StopTyping,
+			prev: undefined
+		});
+		return client.window.document.getElementById(aesAccess).parentElement.outerHTML === `<details open=""><summary>${aesAccess}</summary><div class="chatButtonsContainer"><input type="button" value="Clear Chat Locally" class="chatButtons"><input type="button" value="Clear Chat Globally" class="chatButtons"><input type="button" value="Generate New AES Key" class="chatButtons"><input type="button" value="Upload File" class="chatButtons"><input type="button" value="Share Location" class="chatButtons"><label>Send Typing Indicators</label><input type="checkbox" class="chatButtons"></div><span class="message" id="${aesAccess}"></span><div class="chatButtonsContainer"><textarea class="sendBar"></textarea><input type="button" value=">" class="sendButton"></div></details>`;
+	},
 }).map(async ([key, value]: [string, () => Promise<boolean>]): Promise<void> => {
 	if (!await value()) {
 		console.error(`Failed Test: ${key}`);
