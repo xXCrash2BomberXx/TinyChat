7929506f28a6a2d672f29e9e19ee9afbb9686124
bleesman21 <bradley.leesman@slu.edu>
2024-01-01 11:43:03 -0600

    Still broken

--- TinyChat.ts
+++ TinyChat.ts
@@ -1348,11 +1348,10 @@ class Client {
 	 * @returns {string} a `string` of the Encrypted message.
 	 */
 	async #encryptAES(aesAccess: string, message: string): Promise<string> {
-		this.#window.atob
-		return message ? this.#window.btoa(String.fromCharCode(...new Uint8Array(await this.#crypto.subtle.encrypt(
+		return message ? new TextDecoder().decode(new Uint8Array(await this.#crypto.subtle.encrypt(
 			{ name: 'AES-CBC', iv: this.#aesKeys[aesAccess][0] },
 			this.#aesKeys[aesAccess][1],
-			new Uint8Array(this.#str2ab(this.#window.atob(message))))))) : message;
+			new TextEncoder().encode(message)))) : message;
 	}
 
 	/**
@@ -1362,10 +1361,10 @@ class Client {
 	 * @returns {string} a `string` of the Decrypted message.
 	 */
 	async #decryptAES(aesAccess: string, message: string): Promise<string> {
-		return message ? this.#window.btoa(String.fromCharCode(...new Uint8Array(await this.#crypto.subtle.decrypt(
+		return message ? new TextDecoder().decode(new Uint8Array(await this.#crypto.subtle.decrypt(
 			{ name: 'AES-CBC', iv: this.#aesKeys[aesAccess][0] },
 			this.#aesKeys[aesAccess][1],
-			new Uint8Array(this.#str2ab(this.#window.atob(message))))))) : message;
+			new TextEncoder().encode(message)))) : message;
 	}
 
 	/**
@@ -1414,10 +1413,10 @@ class Client {
 	 * @returns {string} a `string` of the Encrypted message.
 	 */
 	async #encryptRSA(publicKey: CryptoKey, message: string): Promise<string> {
-		return message ? this.#window.btoa(String.fromCharCode(...new Uint8Array(await this.#crypto.subtle.encrypt(
+		return message ? new TextDecoder().decode(new Uint8Array(await this.#crypto.subtle.encrypt(
 			{ name: 'RSA-OAEP' },
 			publicKey,
-			new Uint8Array(this.#str2ab(this.#window.atob(message))))))) : message;
+			new TextEncoder().encode(message)))) : message;
 	}
 
 	/**
@@ -1426,10 +1425,10 @@ class Client {
 	 * @returns {string} a `string` of the Decrypted message.
 	 */
 	async #decryptRSA(message: string): Promise<string> {
-		return message ? this.#window.btoa(String.fromCharCode(...new Uint8Array(await this.#crypto.subtle.decrypt(
+		return message ? new TextDecoder().decode(new Uint8Array(await this.#crypto.subtle.decrypt(
 			{ name: 'RSA-OAEP' },
 			(await this.#keyPair).privateKey,
-			new Uint8Array(this.#str2ab(this.#window.atob(message))))))) : message;
+			new TextEncoder().encode(message)))) : message;
 	}
 
 	get window(): Window {
